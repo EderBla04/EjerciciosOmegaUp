03056481556edaa43ee1683243287bcf2594d204
EderBla04 <a2233336115@alumnos.uat.edu.mx>
2024-12-13 20:14:44 -0600

    mejorar mi logica con los strings

--- FormadoEnLaCafeteria.cpp
+++ FormadoEnLaCafeteria.cpp
@@ -7,7 +7,56 @@ using namespace std;
 int main() {
     FastIO;
 
+    int n;
+
+    string nombre, caso, linea, aux;
+
+    queue<string> alumnos, trabajadores;
+
+    cin>>n;
+    cin.ignore();
+
+    while (n--) {
+
+        getline(cin, linea);
+        stringstream ss(linea);
+
+        ss >> caso;
+
+        if(caso=="ATIENDE"){
 
 
+            if (trabajadores.empty()) {
+
+                cout<<alumnos.front()<<"\n";
+                alumnos.pop();
+            }
+            else {
+
+                cout<<trabajadores.front()<<"\n";
+                trabajadores.pop();
+            }
+        }
+        else {
+
+            ss >> aux;
+            caso+= ' '+ aux;
+
+            if (caso=="LLEGA ALUMNO") {
+                aux.clear();
+                caso.clear();
+                ss >> nombre;
+                alumnos.push(nombre);
+            }
+
+            else if (caso=="LLEGA TRABAJADOR") {
+                aux.clear();
+                caso.clear();
+                ss >> nombre;
+                trabajadores.push(nombre);
+            }
+
+        }
+    }
     return 0;
 }
